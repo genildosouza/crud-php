94f653a1108964ce8becb9672d3cb92110e13474
genildosouza <jgmicroshs@mail.com>
2022-02-16 18:14:26 -0300

    modified files folder

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
   <title>Crud-PhP</title>
 </head>
+
 <body>
 
-<h3>Crud em PHP<3/h>
+  <h3>Crud em PHP<3 /h>
 
 
+
+
+
+
+
+
+
+
+
+
+
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
